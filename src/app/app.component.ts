a00f34394c56fd0d54dd27f8e66d3172ab80dda4
Wesley Alves <wesleyspa@yahoo.com.br>
2024-10-11 11:15:19 -0300

    Promises

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'devio-RXJS';
+
+  ngOnInit(): void {
+    this.myPromise('Js')
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => console.log(error));
+  }
+
+  myPromise(name: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      if (name == 'Wesley') {
+        setTimeout(() => {
+          resolve('Seja bem vindo, Wesley');
+        }, 1000);
+      } else {
+        reject('Você não é o Wesley!');
+      }
+    });
+  }
 }
